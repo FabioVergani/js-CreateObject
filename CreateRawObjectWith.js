dde392f01845a03a3dcce27126ed7c499be81c69
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-04-07 18:00:03 +0200

    Update CreateRawObjectWith.js

--- CreateRawObjectWith.js
+++ CreateRawObjectWith.js
@@ -6,12 +6,12 @@ CreateRawObject=Call.bind(Create,null,null),
 CreateRawObjectWith=function(x){var e,o=x,p,i=this,t=i.t,f=i.f,d=f();for(p in o){i=d[p]=f(t);e=o[p];for(p in e){i[p]=e[p];};};return f(d);}.bind(
  Object.freeze({
   f:CreateRawObject,
-  t:{
-	value:{value:undefined,writable:true,configurable:false,enumerable:true},
-	configurable:{value:true,writable:true,configurable:false},
-	enumerable:{value:true,writable:true,configurable:false},
-	writable:{value:true,writable:true,configurable:false}
-  }
+  t:(function(x){
+	 var f=Create,e=x,o={value:true,writable:true,configurable:false,enumerable:true},p;
+	 for(p in o){e[p]=f(o)};
+	 e.value.value=undefined;
+	 return e;
+	})(CreateRawObject())
  })
 );
 //================
