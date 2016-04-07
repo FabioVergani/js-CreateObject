68771456ae5dba11be4657a45de0b1931ad91ecc
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-04-07 16:56:54 +0200

    Update maker.js

--- maker.js
+++ maker.js
@@ -3,7 +3,7 @@ Call=w.Function.prototype.call,
 Object=w.Object,
 Create=Object.create,
 CreateRawObject=Call.bind(Create,null,null),
-CreateRawObject_AsDescribed=function(x){var e,o=x,p,i=this,t=i.t,f=i.f,d=f();for(p in o){i=d[p]=f(t);e=o[p];for(p in e){i[p]=e[p];};};return f(d);}.bind(
+CreateRawObjectWith=function(x){var e,o=x,p,i=this,t=i.t,f=i.f,d=f();for(p in o){i=d[p]=f(t);e=o[p];for(p in e){i[p]=e[p];};};return f(d);}.bind(
  Object.freeze({
   f:CreateRawObject,
   t:{
@@ -15,7 +15,7 @@ CreateRawObject_AsDescribed=function(x){var e,o=x,p,i=this,t=i.t,f=i.f,d=f();for
  })
 );
 //================
-console.dir(CreateRawObject_AsDescribed({
+console.dir(CreateRawObjectWith({
 name1:{value:'1',enumerable:false},
 name2:{value:2},
 name3:{value:false},
