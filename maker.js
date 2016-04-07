431221cb16373b5fec5dfb907a4113d717458563
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-04-07 16:41:21 +0200

    Update maker.js

--- maker.js
+++ maker.js
@@ -1,14 +1,11 @@
 var w=window,
 Call=w.Function.prototype.call,
 Object=w.Object,
-Create=Call.bind(Object.create,null,null),
-CreateByDescriptor=function(x){
- var e,o=x,p,i=this,t=i.t,f=i.f,d=f();
- for(p in o){i=d[p]=f(t);e=o[p];for(p in e){i[p]=e[p];};};
- return f(d);
-}.bind(
+Create=Object.create,
+CreateRawObject=Call.bind(Create,null,null),
+CreateRawObject_AsDescribed=function(x){var e,o=x,p,i=this,t=i.t,f=i.f,d=f();for(p in o){i=d[p]=f(t);e=o[p];for(p in e){i[p]=e[p];};};return f(d);}.bind(
  Object.freeze({
-  f:Create,
+  f:CreateRawObject,
   t:{
 	value:{value:undefined,writable:true,configurable:false,enumerable:true},
 	configurable:{value:true,writable:true,configurable:false},
@@ -18,7 +15,7 @@ CreateByDescriptor=function(x){
  })
 );
 //================
-console.dir(CreateByDescriptor({
+console.dir(CreateRawObject_AsDescribed({
 name1:{value:'1',enumerable:false},
 name2:{value:2},
 name3:{value:false},
